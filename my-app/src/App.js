a411c9b6ce9dc5060ac172393251d3893fd27cee
Divya <d.prasad.8018@gmail.com>
2024-07-27 17:04:18 -0600

    install tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <h1 className="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
     </div>
   );
 }
