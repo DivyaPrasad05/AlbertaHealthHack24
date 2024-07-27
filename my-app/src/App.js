14aa87bcb4ede1fecdda61f28437a6f7726b8ae4
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-07-27 14:55:25 -0600

    chore: Add react-router-dom package and updated App.js and index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Home from './Pages/Home';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
