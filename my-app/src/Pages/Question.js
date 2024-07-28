2bf4c3d24a44f18395e2610ecd67620fbf891884
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-07-27 21:22:57 -0600

    fix the width to stop bleeding over

--- my-app/src/Pages/Question.js
+++ my-app/src/Pages/Question.js
@@ -3,19 +3,20 @@ const Question = () => {
     return (
         <div className="h-screen">
             <NavBar />
-
-            <div className="ml-10 w-full sm:max-w-xl sm:ml-56">
-                <img
-                    src="/chatbox.png"
-                    alt="chatbox"
-                />
-            </div>
-            <div className="ml-5 sm:ml-36">
-                <img
-                    src="/po1.png"
-                    alt="Panda"
-                    className="w-32 sm:w-48"
-                />
+            <div>
+                <div className="ml-10 w-5/6 sm:max-w-xl sm:ml-56">
+                    <img
+                        src="/chatbox.png"
+                        alt="chatbox"
+                    />
+                </div>
+                <div className="ml-5 sm:ml-36">
+                    <img
+                        src="/po1.png"
+                        alt="Panda"
+                        className="w-32 sm:w-48"
+                    />
+                </div>
             </div>
         </div>
     );
